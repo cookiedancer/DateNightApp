057710a796a6bb3d52fe40510e07f6173260c973
Paul Frazier <paul.g.frazier@gmail.com>
2016-04-22 12:14:31 -0400

    Added files via upload

--- Gamescreen.java
+++ Gamescreen.java
@@ -18,12 +18,78 @@ public class Gamescreen extends Actor
     {//Start Method
         if(Greenfoot.isKeyDown("1"))
         setImage("Hispanic background.jpg"); 
-        Greenfoot.delay(5);
+        Greenfoot.delay(70);
+        setImage("Gamescreen.jpg");
+ 
+        if(Greenfoot.isKeyDown("2"))
+        setImage("Italian background.jpg"); 
+        Greenfoot.delay(70);
         setImage("Gamescreen.jpg");
         
+        if(Greenfoot.isKeyDown("3"))
+        setImage("American background.jpg"); 
+        Greenfoot.delay(70);
+        setImage("Gamescreen.jpg");
+        
+        if(Greenfoot.isKeyDown("4"))
+        setImage("Indian background.jpg"); 
+        Greenfoot.delay(70);
+        setImage("Gamescreen.jpg");
+        
+        if(Greenfoot.isKeyDown("5"))
+        setImage("Oriental background.jpg"); 
+        Greenfoot.delay(70);
+        setImage("Gamescreen.jpg");
+        
+        if(Greenfoot.isKeyDown("A"))
+        setImage("Amusement Park.jpg"); 
+        Greenfoot.delay(70);
+        setImage("Gamescreen.jpg");
+        
+        if(Greenfoot.isKeyDown("B"))
+        setImage("Beach.jpg"); 
+        Greenfoot.delay(70);
+        setImage("Gamescreen.jpg");
+        
+        if(Greenfoot.isKeyDown("C"))
+        setImage("Home.jpg"); 
+        Greenfoot.delay(70);
+        setImage("Gamescreen.jpg");
+        
+        if(Greenfoot.isKeyDown("D"))
+        setImage("Park.jpg"); 
+        Greenfoot.delay(70);
+        setImage("Gamescreen.jpg");
+        
+        if(Greenfoot.isKeyDown("E"))
+        setImage("Zoo.jpg"); 
+        Greenfoot.delay(70);
+        setImage("Gamescreen.jpg");
+        
+        if(Greenfoot.isKeyDown("6"))
+        setImage("Action.jpg"); 
+        Greenfoot.delay(70);
+        setImage("Gamescreen.jpg");
+        
+        if(Greenfoot.isKeyDown("7"))
+        setImage("Comedy.jpg"); 
+        Greenfoot.delay(70);
+        setImage("Gamescreen.jpg");
+        
+        if(Greenfoot.isKeyDown("8"))
+        setImage("Horror.jpg"); 
+        Greenfoot.delay(70);
+        setImage("Gamescreen.jpg");
+        
+        if(Greenfoot.isKeyDown("9"))
+        setImage("Musical.jpg"); 
+        Greenfoot.delay(70);
+        setImage("Gamescreen.jpg");
+        
+        if(Greenfoot.isKeyDown("0"))
+        setImage("Romance.jpg"); 
+        Greenfoot.delay(70);
+        setImage("Gamescreen.jpg");
     }//End Method
-    
-    
-    
-    
+        
 }//End Class
