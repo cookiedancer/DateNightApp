14ca0990e8bc5af6ab18c321da9b256866103938
Gewelles19 <gr2874@email.vccs.edu>
2016-05-05 18:15:51 -0400

    Added files via upload
    key down methods now works just waiting on paul for new game screen then we will compile

--- Gameshow.java
+++ Gameshow.java
@@ -14,7 +14,7 @@ public class Gameshow extends StartScreen
      * 
      */
     
-    public Gameshow()
+       public Gameshow()
     {//Start Method
         Gamescreen Board = new Gamescreen();
         addObject(Board, 480,320);
@@ -23,6 +23,98 @@ public class Gameshow extends StartScreen
     public void act()
     {//game music method
         myMusic.play();
-    }
+     
+        
+        if(Greenfoot.isKeyDown("1"))
+        addObject(new Food1(), 480,320);
+         
+        if(Greenfoot.isKeyDown("2"))
+        addObject(new Food2(), 480,320);
+        
+        if(Greenfoot.isKeyDown("3"))
+        addObject(new Food3(), 480,320);
+        
+        if(Greenfoot.isKeyDown("A"))
+        addObject(new Food4(), 480,320);
+        
+        if(Greenfoot.isKeyDown("B"))
+        addObject(new Food5(), 480,320);
+        
+        if(Greenfoot.isKeyDown("C"))
+        addObject(new Beach(), 480,320);
+                
+        if(Greenfoot.isKeyDown("D"))
+        addObject(new Home(), 480,320);
+                
+        if(Greenfoot.isKeyDown("E"))
+        addObject(new Amusement(), 480,320);
 
-}
+        if(Greenfoot.isKeyDown("F"))
+        addObject(new Park(), 480,320);
+                
+        if(Greenfoot.isKeyDown("G"))
+        addObject(new Zoo(), 480,320);
+                
+        if(Greenfoot.isKeyDown("H"))
+        addObject(new Comedy(), 480,320);
+        
+        if(Greenfoot.isKeyDown("I"))
+        addObject(new Adventure(), 480,320);
+                
+        if(Greenfoot.isKeyDown("J"))
+        addObject(new Horror(), 480,320);
+                
+        if(Greenfoot.isKeyDown("K"))
+        addObject(new Romance(), 480,320);
+                
+        if(Greenfoot.isKeyDown("L"))
+        addObject(new Drama(), 480,320);
+        
+        if(Greenfoot.isKeyDown("M"))
+        addObject(new Musical(), 480,320);
+                
+        if(Greenfoot.isKeyDown("N"))
+        addObject(new Carnival(), 480,320);
+        
+        if(Greenfoot.isKeyDown("O"))
+        addObject(new Hiking(), 480,320);
+        
+        if(Greenfoot.isKeyDown("P"))
+        addObject(new Play(), 480,320);
+                
+        if(Greenfoot.isKeyDown("Q"))
+        addObject(new Bar(), 480,320);
+        
+        if(Greenfoot.isKeyDown("R"))
+        addObject(new Board(), 480,320);
+        
+        if(Greenfoot.isKeyDown("S"))
+        addObject(new Golf(), 480,320);
+                
+        if(Greenfoot.isKeyDown("T"))
+        addObject(new Card(), 480,320);
+                
+        if(Greenfoot.isKeyDown("U"))
+        addObject(new Arcade(), 480,320);
+                
+        if(Greenfoot.isKeyDown("V"))
+        addObject(new Video(), 480,320);
+        
+        if(Greenfoot.isKeyDown("W"))
+        addObject(new Adventure2(), 480,320);
+                
+        if(Greenfoot.isKeyDown("X"))
+        addObject(new Seafood(), 480,320);
+        
+        if(Greenfoot.isKeyDown("Y"))
+        addObject(new Puzzle(), 480,320);
+                
+        if(Greenfoot.isKeyDown("Z"))
+        addObject(new Chinese(), 480,320);
+            
+    }
+        
+        }
+
+
+
